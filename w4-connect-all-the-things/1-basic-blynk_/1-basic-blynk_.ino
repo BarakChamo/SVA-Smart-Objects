2fcbdfbd4fbc6b65669b903976818af5e4ea10b3
Barak Chamo <BarakChamo@users.noreply.github.com>
2021-02-07 12:35:14 -0500

    Update 1-basic-blynk_.ino

--- w4-connect-all-the-things/1-basic-blynk_/1-basic-blynk_.ino
+++ w4-connect-all-the-things/1-basic-blynk_/1-basic-blynk_.ino
@@ -28,20 +28,20 @@ int ledValue = 0;
 
 // define a timer callback
 // this is where we write to Blynk virtual pins
-//void onTimer() {
-//  // Write to virtual pins.
-//  // don't send more that 10 values per second.
-//  Blynk.virtualWrite(V5, potValue);
-//  Blynk.virtualWrite(V6, buttonValue);
-//  Blynk.virtualWrite(V7, buttonValue * 255); // write to virtual LED pin
-//}
+void onTimer() {
+  // Write to virtual pins.
+  // don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, potValue);
+  Blynk.virtualWrite(V6, buttonValue);
+  Blynk.virtualWrite(V7, buttonValue * 255); // write to virtual LED pin
+}
 
 // Listen for changes of virtual pin 4
 // BLYNK_WRITE are special functions that can be defined multiple times with different pin arguments
-//BLYNK_WRITE(V4) {
-//  ledValue = param.asInt();
-//  analogWrite(ledPin, ledValue);
-//}
+BLYNK_WRITE(V4) {
+  ledValue = param.asInt();
+  analogWrite(ledPin, ledValue);
+}
 
 void setup() {
   // Debug console
@@ -51,7 +51,7 @@ void setup() {
   Blynk.begin(auth, ssid, pass);
 
   // configure the blynk timer to call onTimer() every second
-//  timer.setInterval(1000L, onTimer);
+  timer.setInterval(1000L, onTimer);
 
   // setup input pins
   pinMode(potPin, INPUT);
@@ -63,18 +63,18 @@ void loop() {
   Blynk.run();
 
   // update the Blynk timer
-//  timer.run();
+  timer.run();
   
   // read physical pins
   potValue = analogRead(potPin);
   int tempButtonValue = digitalRead(buttonPin);
 
   // if button is pressed, send push notification to Blynk app
-//  if(tempButtonValue && !buttonValue) {
-//    // careful! notice we're only writing to Blynk in loop() under certain conditions
-//    Blynk.notify("Yaaay... button is pressed!");
-//    Blynk.virtualWrite(V8, "Hello from my Feather!"); // write to virtual webhook pin
-//  }
+  if(tempButtonValue && !buttonValue) {
+    // careful! notice we're only writing to Blynk in loop() under certain conditions
+    Blynk.notify("Yaaay... button is pressed!");
+    Blynk.virtualWrite(V8, "Hello from my Feather!"); // write to virtual webhook pin
+  }
   
   // keep track of latest button value
   buttonValue = tempButtonValue;
