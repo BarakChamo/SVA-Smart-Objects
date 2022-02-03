1aff26ba556e1f120a8be7048e94e08f3a3c05c4
Barak Chamo <BarakChamo@users.noreply.github.com>
2022-02-03 00:36:53 -0500

    Update 1-basic-blynk_.ino

--- w4-connect-all-the-things/1-basic-blynk_/1-basic-blynk_.ino
+++ w4-connect-all-the-things/1-basic-blynk_/1-basic-blynk_.ino
@@ -1,75 +1,102 @@
-/*************************************************************
+#include <analogWrite.h>
 
-  This is a simple demo of sending and receiving some data.
-  Be sure to check out other examples!
- *************************************************************/
-
-// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
-// See the Device Info tab, or Template settings
-#define BLYNK_TEMPLATE_ID "TMPLllqKdIoF"
-#define BLYNK_DEVICE_NAME "Simple Template"
-#define BLYNK_AUTH_TOKEN "YOUR AUTH TOKEN"
-
-
-// Comment this out to disable prints and save space
+/* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
-
-#define LED_ANALOG 25
-#define LED_DIGITAL 26
-
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
-#include <analogWrite.h>
 
-char auth[] = BLYNK_AUTH_TOKEN;
+const int potPin = A4;
+const int buttonPin = 14;
+const int ledPin = A1;
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "BLYNK_AUTH";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "YOUR WIFI SSID";
-char pass[] = "YOUR WIFI PASS";
+char ssid[] = "WIFI_SSID";
+char pass[] = "WIFI_PASS";
 
 
-// This function is called every time the Virtual Pin 0 state changes
-BLYNK_WRITE(V0)
-{
-  // Set incoming value from pin V0 to a variable
-  int value = param.asInt();
-  Serial.println(value);
-  digitalWrite(LED_DIGITAL, value);
-  
-  // Update state
-  Blynk.virtualWrite(V1, value);
+BlynkTimer timer;
+
+int potValue = 0;
+int buttonValue = 0;
+int ledValue = 0;
+
+// define a timer callback
+// this is where we write to Blynk virtual pins
+void onTimer() {
+  // Write to virtual pins.
+  // don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, potValue);
+  Blynk.virtualWrite(V6, buttonValue);
+  Blynk.virtualWrite(V7, buttonValue * 255); // write to virtual LED pin
 }
 
-// This function is called every time the Virtual Pin 0 state changes
-BLYNK_WRITE(V1)
-{
-  // Set incoming value from pin V0 to a variable
-  int value = param.asInt();
-  Serial.println(value);
 
-  analogWrite(LED_ANALOG, value);
-
+// called when the device is connected to the Blynk server
+BLYNK_CONNECTED() {
+  // Your code here
 }
 
-// This function is called every time the device is connected to the Blynk.Cloud
-BLYNK_CONNECTED()
-{
+// called when an app is connected to the Blynk project
+BLYNK_APP_CONNECTED() {
+  // Your code goes here
 }
 
-void setup()
-{
+// called when an app is disconnceted from the Blynk project
+BLYNK_APP_DISCONNECTED(){
+}
+
+// Listen for changes of virtual pin 4
+// BLYNK_WRITE are special functions that can be defined multiple times with different pin arguments
+BLYNK_WRITE(V4) {
+  ledValue = param.asInt();
+  analogWrite(ledPin, ledValue);
+}
+
+// for more Blynk firmware handlers visit http://docs.blynk.cc/#blynk-firmware
+
+void setup() {
   // Debug console
-  Serial.begin(115200);
-  pinMode(LED_DIGITAL, OUTPUT);
+  Serial.begin(9600);
+
+  // start the Blink service over WiFi
   Blynk.begin(auth, ssid, pass);
-  // You can also specify server:
-  //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
-  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  // configure the blynk timer to call onTimer() every second
+  timer.setInterval(1000L, onTimer);
+
+  // setup input pins
+  pinMode(potPin, INPUT);
+  pinMode(buttonPin, INPUT);
 }
 
-void loop()
-{
+void loop() {
+  // update Blynk connection and automatic values
   Blynk.run();
+
+  // update the Blynk timer
+  timer.run();
+  
+  // read physical pins
+  potValue = analogRead(potPin);
+  int tempButtonValue = digitalRead(buttonPin);
+
+  // if button is pressed, send push notification to Blynk app
+  if(tempButtonValue && !buttonValue) {
+    // careful! notice we're only writing to Blynk in loop() under certain conditions
+    Blynk.notify("Yaaay... button is pressed!");
+    Blynk.virtualWrite(V8, "Hello from Barak's Feather!"); // write to virtual webhook pin
+    Blynk.virtualWrite(V8, "Hello from my Feather!"); // write to virtual webhook pin
+  }
+  
+  // keep track of latest button value
+  buttonValue = tempButtonValue;
+
+  // do anything else you want ion your sketch
+  // but, DO NOT WRITE TO BLYNK HERE CONTINUOUSLY, ONLY IN TIMEOUT HANDLER!
 }
